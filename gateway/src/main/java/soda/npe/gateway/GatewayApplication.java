dec7d93d28c4c6378d96327536ae719a1d5dc80c
Sodacooky <sodacooky@foxmail.com>
2023-04-20 20:13:01 +0800

    修改： 优化项目结构，通过common模块里的common profile配置所有模块的数据源、Eureka、Redis、Jackson日期序列化配置等； 移除了所有单元测试及其目录，我们不要单元测试，也不会测；

--- gateway/src/main/java/soda/npe/gateway/GatewayApplication.java
+++ gateway/src/main/java/soda/npe/gateway/GatewayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"soda.npe.gateway", "soda.npe.common"})
 @EnableDiscoveryClient
 public class GatewayApplication {
 
---
+++ gateway/src/main/java/soda/npe/gateway/UserTokenFilter.java
@@ -0,0 +1,52 @@
+package soda.npe.gateway;
+
+import jakarta.annotation.Resource;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import soda.npe.common.utils.JwtAuthUtil;
+
+/**
+ * 用户登录拦截Token校验过滤器
+ */
+@Component
+public class UserTokenFilter implements GlobalFilter, Ordered {
+
+    @Resource
+    private JwtAuthUtil jwtAuthUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //判断是否是用户，即非/admin/开头，不是用户方面接口则跳过放行（其实先执行了admin的过滤器
+        if (exchange.getRequest().getPath().toString().startsWith("/admin")) {
+            return chain.filter(exchange);
+        }
+        //判断当前访问的端口是否需要鉴权，目前，我们约定用户需要登录的接口都在/operation下，
+        //并且，SpringCloud Gateway的GetPath()，上面那样，并不会获得对应微服务的名称！
+        if (!exchange.getRequest().getPath().toString().startsWith("/operation")) {
+            //不需要鉴权
+            return chain.filter(exchange);
+        }
+        //鉴权，读取请求中的token
+        String jwt = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (jwt == null || !jwtAuthUtil.validation(jwt)) {
+            //没有token或者校验失败，设置498并且立刻返回
+            // - 设置Response的状态码
+            exchange.getResponse().setStatusCode(HttpStatusCode.valueOf(498));
+            // - 过滤器链不继续往下传，立刻返回
+            return exchange.getResponse().setComplete();
+        } else {
+            //正常放行
+            return chain.filter(exchange);
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return 1; //不设置为0,0是admin，因为/admin和/我们不希望/admin走到/里面去
+    }
+}
